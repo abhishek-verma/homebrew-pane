9cd2bf8bef924a1dab2a107bac0b87d9cecf5f5b
Abhishek Verma <inpen.abhi@gmail.com>
2026-08-12 10:04:06 +0530

    chore: update pane cask to v0.47.0.78

--- Casks/pane.rb
+++ Casks/pane.rb
@@ -1,9 +1,9 @@
 cask "pane" do
-  version "0.47.0.77"
+  version "0.47.0.78"
 
   on_arm do
     url "https://github.com/abhishek-verma/Pane/releases/download/v\#{version}/Pane_v\#{version}_arm64.dmg"
-    sha256 "8b2787b8682733b3b31630a5fa29dbd68eb7197542518854a425a5ccef927a5b"
+    sha256 "76c23c1bcd77adb0601638444e4613e46697c424e1e43b0b86ebc91cf038d450"
   end
 
   name "Pane"
